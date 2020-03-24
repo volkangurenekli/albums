f57d23beb17527996a68e15e4b87e50ce1d1b19b
volkangurenekli <tcsgvolkan@gmail.com>
2020-03-24 08:51:01 +0300

    displaying album artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = props => {
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image, image} = album;
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle,
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{uri: thumbnail_image}} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{artist}</Text>
+          <Text>{title}</Text>
+        </View>
       </CardSection>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <Image style={imageStyle} source={{uri: image}} />
       </CardSection>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <Text>{title}</Text>
       </CardSection>
     </Card>
   );
@@ -21,4 +35,27 @@ const AlbumDetail = props => {
 
 export default AlbumDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
+});
